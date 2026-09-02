1803e0484176bd8f45994b00cb22e05a9291f9f0
pavelafonicev61-ctrl <pavelafonicev61@gmail.com>
2026-09-02 21:14:00 +0300

    Update RedstoynMenButterfliesClient.java

--- redstoynmen_butterflies_FIXED_FOR_GITHUB/src/client/java/com/redstoynmen/butterflies/client/RedstoynMenButterfliesClient.java
+++ redstoynmen_butterflies_FIXED_FOR_GITHUB/src/client/java/com/redstoynmen/butterflies/client/RedstoynMenButterfliesClient.java
@@ -5,7 +5,7 @@ import com.redstoynmen.butterflies.RedstoynMenButterflies;
 import com.redstoynmen.butterflies.client.render.ButterflyModel;
 import com.redstoynmen.butterflies.client.render.ButterflyRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRenderers;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.ModelLayerRegistry;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.resources.Identifier;
@@ -17,8 +17,9 @@ public final class RedstoynMenButterfliesClient implements ClientModInitializer 
     @Override
     public void onInitializeClient() {
         ModelLayerRegistry.registerModelLayer(BUTTERFLY, ButterflyModel::createBodyLayer);
-        EntityRenderers.register(ModEntityTypes.COMMON_BUTTERFLY, ButterflyRenderer::new);
-        EntityRenderers.register(ModEntityTypes.GREEN_BUTTERFLY, ButterflyRenderer::new);
-        EntityRenderers.register(ModEntityTypes.RED_BUTTERFLY, ButterflyRenderer::new);
+
+        EntityRendererRegistry.register(ModEntityTypes.COMMON_BUTTERFLY, ButterflyRenderer::new);
+        EntityRendererRegistry.register(ModEntityTypes.GREEN_BUTTERFLY, ButterflyRenderer::new);
+        EntityRendererRegistry.register(ModEntityTypes.RED_BUTTERFLY, ButterflyRenderer::new);
     }
 }
